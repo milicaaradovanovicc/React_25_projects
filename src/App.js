c4eabd5580c3134bf1e5962652c278f164677d62
Milica Radovanovic <milicaa.radovanovicc@gmail.com>
2024-06-06 13:41:16 +0200

    load more

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Accordian from "./components/accordion";
 import ImageSlider from "./components/image-slider";
+import LoadMoreData from "./components/load-more-data";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
 
@@ -17,11 +18,14 @@ function App() {
       {/* <StarRating noOfStars={10} /> */}
 
       {/* image slider component */}
-      <ImageSlider
+      {/* <ImageSlider
         url={"https://picsum.photos/v2/list"}
         limit={"10"}
         page={"1"}
-      />
+      /> */}
+
+      {/* Load more component */}
+      <LoadMoreData />
     </div>
   );
 }
