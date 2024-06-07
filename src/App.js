9ad861afac021953ee3b7d3add28eb121691aeac
Milica Radovanovic <milicaa.radovanovicc@gmail.com>
2024-06-07 16:06:08 +0200

    scroll progress

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import TreeView from "./components/tree-view";
 import menus from "./components/tree-view/data";
 import QRCodeGenerator from "./components/qr-code-generator";
 import LightDarkMode from "./components/light-dark-mode";
+import ScrollIndicator from "./components/scroll-indicator";
 
 function App() {
   return (
@@ -38,7 +39,10 @@ function App() {
       {/* <QRCodeGenerator /> */}
 
       {/* Light dark mode */}
-      <LightDarkMode />
+      {/* <LightDarkMode /> */}
+
+      {/* Scroll indicator component */}
+      <ScrollIndicator url={"https://dummyjson.com/products?limit=100"} />
     </div>
   );
 }
