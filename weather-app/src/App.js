1daa2e4e211790ec51dcc8baad7ac3561ec8ad30
Milica Radovanovic <milicaa.radovanovicc@gmail.com>
2024-06-18 14:39:32 +0200

    weather app

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Weather from "./components/weather";
 
 function App() {
   return (
     <div className="App">
-      
+      <Weather />
     </div>
   );
 }
