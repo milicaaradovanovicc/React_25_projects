8af34e96d18b058d8f6c3c6972fd092021170266
Milica Radovanovic <milicaa.radovanovicc@gmail.com>
2024-06-24 16:39:58 +0200

    shopping cart

---
+++ shoping-cart-app/src/pages/cart.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import CartTile from "../components/cart-tile";
+
+export default function Cart() {
+  const [totalCart, setTotalCart] = useState(0);
+
+  const { cart } = useSelector((state) => state);
+
+  useEffect(() => {
+    setTotalCart(cart.reduce((acc, curr) => acc + curr.price, 0));
+  }, [cart]);
+
+  return (
+    <div>
+      {cart && cart.length ? (
+        <div className="flex justify-center">
+          <div className="min-h-[80vh] grid md:grid-cols-2 max-w-6xl mx-auto">
+            <div className="flex flex-col justify-center items-center p-3">
+              {cart.map((cartItem) => (
+                <CartTile cartItem={cartItem} />
+              ))}
+            </div>
+          </div>
+          <div className="w-[500px]">
+            <div className="flex flex-col justify-center items-end p-5 space-y-5 mt-5 bg-gray-200 rounded-xl">
+              <h1 className="font-bold text-lg text-red-800">
+                Your cart summary
+              </h1>
+              <p>
+                <span className="text-grey-800 font-bold">Total item</span>
+                <span>: {cart.length}</span>
+              </p>
+              <p>
+                <span className="text-grey-800 font-bold">Total amount</span>
+                <span>: {totalCart}$</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="min-h-[80vh] flex flex-col items-center justify-center">
+          <h1 className="text-gray-800 font-bold text-xl mb-2">
+            Your cart is empty
+          </h1>
+          <Link to="/">
+            <button className="bg-red-950 text-white border-2 rounded-lg font-bold p-4">
+              Shop now
+            </button>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+}
